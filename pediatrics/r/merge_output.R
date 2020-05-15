0b4b1c5e4280dc0d86e767d254c80236c663e638
LEE CHUNGGAK <lechga@naver.com>
2020-05-15 15:46:42 +0900

    Update merge_output.R

--- pediatrics/r/merge_output.R
+++ pediatrics/r/merge_output.R
@@ -9,7 +9,7 @@ source("E:/Users/DLCG001/workspace/ltool/ltool.R")
 
 
 # Make directory ----------------------------------------------------------
-output_path <- "merged_output_v2"
+output_path <- "merged_output_v3"
 desc_stat_path <- file.path(output_path, "desc_stat")
 plot_path <- file.path(output_path, "plot")
 
@@ -36,13 +36,24 @@ for (file_var in file_table$file) {
                 mutate(hospital = hospital_var) %>% 
                 spread(type, value)
             
+            if (group_key == "DM") {
+                d <- d %>% 
+                    mutate(DM = factor(DM, levels = c("Type 1", "Type 2")))
+            } else if (group_key == "age_group") {
+                d <- d %>% 
+                    mutate(age_group = factor(age_group, levels = c("0-4Y", "5-9Y", "10-14Y")))
+            } else {
+                d <- d %>% 
+                    mutate(sex = factor(sex, levels = c("Male", "Female")))
+            }
+            
             # Make plot per hospital
             for (measurement_var in c("zbmi", "HBA1C")) {
                 make_pediatrics_plot(d = d %>%
                                          filter(measurement == measurement_var) %>%
                                          mutate(time = sprintf("%02d", time)) %>%
                                          as.data.table(),
-                                     p_value_table = fread(file.path(hospital_var, "p-value.csv")) %>%
+                                     p_value_table = fread(file.path(hospital_var, "4_P_Value.csv")) %>%
                                          mutate(time = sprintf("%02d" ,time)) %>%
                                          as.data.table(),
                                      measurement_var = measurement_var,
@@ -61,13 +72,24 @@ for (file_var in file_table$file) {
                 mutate(hospital = hospital_var) %>% 
                 spread(type, value)
             
+            if (group_key == "DM") {
+                d_part <- d_part %>% 
+                    mutate(DM = factor(DM, levels = c("Type 1", "Type 2")))
+            } else if (group_key == "age_group") {
+                d_part <- d_part %>% 
+                    mutate(age_group = factor(age_group, levels = c("0-4Y", "5-9Y", "10-14Y")))
+            } else {
+                d_part <- d_part %>% 
+                    mutate(sex = factor(sex, levels = c("Male", "Female")))
+            }
+            
             # Make plot per hospital
             for (measurement_var in c("zbmi", "HBA1C")) {
                 make_pediatrics_plot(d = d_part %>%
                                          filter(measurement == measurement_var) %>%
                                          mutate(time = sprintf("%02d", time)) %>%
                                          as.data.table(),
-                                     p_value_table = fread(file.path(hospital_var, "p-value.csv")) %>%
+                                     p_value_table = fread(file.path(hospital_var, "4_P_Value.csv")) %>%
                                          mutate(time = sprintf("%02d" ,time)) %>%
                                          as.data.table(),
                                      measurement_var = measurement_var,
@@ -109,6 +131,30 @@ for (file_var in file_table$file) {
         summarise(weighted_mean = weighted.mean(mean, n),
                   n = sum(n))
     
+    if (group_key == "DM") {
+        d_reorder <- d %>% 
+            mutate(DM = ifelse(DM == "Type 1", "Type 1 (n)", "Type 2 (n)"),
+                   DM = factor(DM, levels = c("Type 2 (n)", "Type 1 (n)"))) %>%
+            group_by_at(group_key_vec) %>%
+            summarise(weighted_mean = weighted.mean(mean, n),
+                      n = sum(n))
+    } else if (group_key == "age_group") {
+        d_reorder <- d %>% 
+            mutate(age_group = ifelse(age_group == "0-4Y", "0-4Y (n)",
+                                      ifelse(age_group == "5-9Y", "5-9Y (n)", "10-14Y (n)")),
+                   age_group = factor(age_group, levels = c("10-14Y (n)", "5-9Y (n)", "0-4Y (n)"))) %>%
+            group_by_at(group_key_vec) %>%
+            summarise(weighted_mean = weighted.mean(mean, n),
+                      n = sum(n))
+    } else {
+        d_reorder <- d %>% 
+            mutate(sex = ifelse(sex == "Male", "Male (n)", "Female (n)"),
+                   sex = factor(sex, levels = c("Female (n)", "Male (n)"))) %>%
+            group_by_at(group_key_vec) %>%
+            summarise(weighted_mean = weighted.mean(mean, n),
+                      n = sum(n))
+    }
+    
     write_csv(d1, file.path(desc_stat_path, paste(str_extract(file_var, "\\w+Group"), "_MergedMeasurementDescStat.csv", sep = "_")))
     
     for (measurement_var in c("zbmi", "HBA1C")) {
@@ -121,7 +167,7 @@ for (file_var in file_table$file) {
             y_lab <- "BMI z score"
         } else {
             hline_y <- 7
-            annotate_y <- 6.3
+            annotate_y <- 6.4
             annotate_label <- "Abnormal(7.0)"
             ylim <- c(4, 12)
             ybreaks <- seq(4, 12, 2)
@@ -178,6 +224,10 @@ for (file_var in file_table$file) {
         }
         
         d2 <- d1 %>% 
+            filter(measurement == measurement_var) %>% 
+            as.data.table()
+        
+        d2_reorder <- d_reorder %>% 
             filter(measurement == measurement_var)
         
         plt <- d2 %>% 
@@ -185,19 +235,21 @@ for (file_var in file_table$file) {
             geom_point(aes_string(shape = group_key), size = 3) +
             geom_line(size = 0.7) +
             geom_hline(yintercept = c(0, hline_y), linetype = "dashed") +
-            annotate("text", x = 7.5, y = annotate_y, label = annotate_label) +
+            annotate("text", x = 7.4, y = annotate_y, label = annotate_label) +
             coord_cartesian(ylim = ylim) +
-            scale_x_discrete(labels = c("03" = expression(bold(underline("03"))),
-                                        "36" = expression(bold(underline("36"))),
-                                        "60" = expression(bold(underline("60"))),
-                                        parse = TRUE)) +
+            # scale_x_discrete(labels = c("03" = expression(bold(underline("03"))),
+            #                             "36" = expression(bold(underline("36"))),
+            #                             "60" = expression(bold(underline("60"))),
+            #                             parse = TRUE)) +
             scale_y_continuous(breaks = ybreaks) +
-            labs(x = "Time(month)", y = y_lab) +
+            labs(x = element_blank(), y = y_lab) +
+            # labs(x = "Time(month)", y = y_lab) +
             theme_classic() +
             theme(legend.position = "top",
                   legend.title = element_blank(),
                   axis.title = element_text(size = 15),
-                  axis.text = element_text(size = 15))
+                  axis.text = element_text(size = 15),
+                  plot.margin = unit(c(10, 10, -2, 10), "mm"))
         
         if (p_value_table[measurement == measurement_var &
                           file == file_var &
@@ -206,7 +258,7 @@ for (file_var in file_table$file) {
                                  file == file_var &
                                  time == "03", p_value][[1]])) {
             plt <- plt +
-                annotate("text", x = "03", y = ylim[2] * 0.8, label = "*", size = 7)
+                annotate("text", x = "03", y = max(d2[time == "03", weighted_mean]) + 1, label = "*", size = 7)
         }
         if (p_value_table[measurement == measurement_var &
                           file == file_var &
@@ -215,7 +267,7 @@ for (file_var in file_table$file) {
                                  file == file_var &
                                  time == "36", p_value][[1]])) {
             plt <- plt + 
-                annotate("text", x = "36", y = ylim[2] * 0.8, label = "*", size = 7)
+                annotate("text", x = "36", y = max(d2[time == "36", weighted_mean]) + 1, label = "*", size = 7)
         }
         if (p_value_table[measurement == measurement_var &
                           file == file_var &
@@ -224,21 +276,32 @@ for (file_var in file_table$file) {
                                  file == file_var &
                                  time == "60", p_value][[1]])) {
             plt <- plt +
-                annotate("text", x = "60", y = ylim[2] * 0.8, label = "*", size = 7)
+                annotate("text", x = "60", y = max(d2[time == "60", weighted_mean]) + 1, label = "*", size = 7)
         }
         
-        df <- d2 %>% 
-            select(-c(measurement, weighted_mean)) %>% 
-            spread(key = time, value = n) %>% 
-            tableGrob(rows = NULL, theme = ttheme_default(core = list(bg_params = list(fill = "white", col = "black")),
-                                                          colhead = list(bg_params = list(col = "black"))))
+        df <-  ggplot(d2_reorder, aes_string(x = "time", y = group_key, label = "n")) +
+            geom_text() +
+            theme_test() +
+            geom_hline(yintercept = 1.5) +
+            geom_vline(xintercept = seq(1.5, 7.5, 1)) +
+            labs(x = "Time(month)", y = element_blank()) +
+            theme(plot.margin = unit(c(0, 10, 10, 5), "mm"),
+                  axis.ticks = element_blank(),
+                  axis.text.x = element_blank(),
+                  axis.title.x = element_text(size = 15))
         
-        g <- grid.arrange(grobs = list(plt, df), nrow = 2, heights = c(2, 1))
+        if (file_var == "Type 1_DM_age_group_Group_Measurement_Desc_Stat.csv") {
+            df <- df +
+                geom_hline(yintercept = 2.5)
+        }
+        
+        g <- grid.arrange(grobs = list(plt, df), nrow = 2, heights = c(2, 0.75))
+        
         ggsave(file.path(plot_path,
                          paste(str_extract(file_var, "\\w+Group"),
                                measurement_var,
                                "Plot.tiff",
-                               sep = "_")), g, dpi = 320, width = 150, height = 150, units = "mm" )
+                               sep = "_")), g, dpi = 320, width = 150, height = 150, units = "mm")
         ggsave(file.path(plot_path,
                          paste(str_extract(file_var, "\\w+Group"),
                                measurement_var,
@@ -246,7 +309,7 @@ for (file_var in file_table$file) {
                                sep = "_")), g, dpi = 320, width = 150, height = 150, units = "mm")
     }
 }
-write_csv(p_value_table, file.path(output_path, "p-value.csv"))
+write_csv(p_value_table, file.path(output_path, "4_P_Value.csv"))
 
 
 # Function make_pediatrics_plot -------------------------------------------
@@ -261,7 +324,7 @@ make_pediatrics_plot <- function(d, p_value_table, measurement_var, group_key,
         y_lab <- "BMI z score"
     } else {
         hline_y <- 7
-        annotate_y <- 6.3
+        annotate_y <- 6.4
         annotate_label <- "Abnormal(7.0)"
         ylim <- c(4, 12)
         ybreaks <- seq(4, 12, 2)
@@ -279,19 +342,21 @@ make_pediatrics_plot <- function(d, p_value_table, measurement_var, group_key,
         geom_point(aes_string(shape = group_key), size = 3) +
         geom_line(size = 0.7) +
         geom_hline(yintercept = c(0, hline_y), linetype = "dashed") +
-        annotate("text", x = 7.5, y = annotate_y, label = annotate_label) +
+        annotate("text", x = 7.4, y = annotate_y, label = annotate_label) +
         coord_cartesian(ylim = ylim) +
-        scale_x_discrete(labels = c("03" = expression(bold(underline("03"))),
-                                    "36" = expression(bold(underline("36"))),
-                                    "60" = expression(bold(underline("60"))),
-                                    parse = TRUE)) +
+        # scale_x_discrete(labels = c("03" = expression(bold(underline("03"))),
+        #                             "36" = expression(bold(underline("36"))),
+        #                             "60" = expression(bold(underline("60"))),
+        #                             parse = TRUE)) +
         scale_y_continuous(breaks = ybreaks) +
-        labs(x = "Time(month)", y = y_lab) +
+        labs(x = element_blank(), y = y_lab) +
+        # labs(x = "Time(month)", y = y_lab) +
         theme_classic() +
         theme(legend.position = "top",
               legend.title = element_blank(),
               axis.title = element_text(size = 15),
-              axis.text = element_text(size = 15))
+              axis.text = element_text(size = 15),
+              plot.margin = unit(c(10, 10, -2, 10), "mm"))
     
     if (p_value_table[DM == str_sub(file_var, 1, 6) &
                       measurement == measurement_var &
@@ -302,7 +367,7 @@ make_pediatrics_plot <- function(d, p_value_table, measurement_var, group_key,
                              group == group_key &
                              time == "03", p_value])) {
         plt <- plt +
-            annotate("text", x = "03", y = ylim[2] * 0.8, label = "*", size = 7)
+            annotate("text", x = "03", y = max(d[time == "03", mean]) + 1, label = "*", size = 7)
     }
     if (p_value_table[DM == str_sub(file_var, 1, 6) &
                       measurement == measurement_var &
@@ -313,7 +378,7 @@ make_pediatrics_plot <- function(d, p_value_table, measurement_var, group_key,
                              group == group_key &
                              time == "36", p_value])) {
         plt <- plt + 
-            annotate("text", x = "36", y = ylim[2] * 0.8, label = "*", size = 7)
+            annotate("text", x = "36", y = max(d[time == "36", mean]) + 1, label = "*", size = 7)
     }
     if (p_value_table[DM == str_sub(file_var, 1, 6) &
                       measurement == measurement_var &
@@ -324,16 +389,47 @@ make_pediatrics_plot <- function(d, p_value_table, measurement_var, group_key,
                              group == group_key &
                              time == "60", p_value])) {
         plt <- plt +
-            annotate("text", x = "60", y = ylim[2] * 0.8, label = "*", size = 7)
+            annotate("text", x = "60", y = max(d[time == "60", mean]) + 1, label = "*", size = 7)
     }
     
-    df <- d %>% 
-        select(!!group_key, time, n) %>% 
-        spread(key = time, value = n) %>% 
-        tableGrob(rows = NULL, theme = ttheme_default(core = list(bg_params = list(fill = "white", col = "black")),
-                                                      colhead = list(bg_params = list(col = "black"))))
+    if (group_key == "DM") {
+        d_reorder <- d %>% 
+            mutate(DM = ifelse(DM == "Type 1", "Type 1 (n)", "Type 2 (n)"),
+                   DM = factor(DM, levels = c("Type 2 (n)", "Type 1 (n)")))
+    } else if (group_key == "age_group") {
+        d_reorder <- d %>% 
+            mutate(age_group = ifelse(age_group == "0-4Y", "0-4Y (n)",
+                                      ifelse(age_group == "5-9Y", "5-9Y (n)", "10-14Y (n)")),
+                   age_group = factor(age_group, levels = c("10-14Y (n)", "5-9Y (n)", "0-4Y (n)")))
+    } else {
+        d_reorder <- d %>% 
+            mutate(sex = ifelse(sex == "Male", "Male (n)", "Female (n)"),
+                   sex = factor(sex, levels = c("Female (n)", "Male (n)")))
+    }
     
-    g <- grid.arrange(grobs = list(plt, df), nrow = 2, heights = c(2, 1))
+    df <-  ggplot(d_reorder, aes_string(x = "time", y = group_key, label = "n")) +
+        geom_text() +
+        theme_test() +
+        geom_hline(yintercept = 1.5) +
+        geom_vline(xintercept = seq(1.5, 7.5, 1)) +
+        labs(x = "Time(month)", y = element_blank()) +
+        theme(plot.margin = unit(c(0, 10, 10, 5), "mm"),
+              axis.ticks = element_blank(),
+              axis.text.x = element_blank(),
+              axis.title.x = element_text(size = 15))
+    
+    if (file_var == "Type 1_DM_age_group_Group_Measurement_Desc_Stat.csv") {
+        df <- df +
+            geom_hline(yintercept = 2.5)
+    }
+    
+    # df <- d %>% 
+    #     select(!!group_key, time, n) %>% 
+    #     spread(key = time, value = n) %>% 
+    #     tableGrob(rows = NULL, theme = ttheme_default(core = list(bg_params = list(fill = "white", col = "black")),
+    #                                                   colhead = list(bg_params = list(col = "black"))))
+    
+    g <- grid.arrange(grobs = list(plt, df), nrow = 2, heights = c(2, 0.75))
     ggsave(paste(file_name, "tiff", sep = "."), g, dpi = 320, width = 150, height = 150, units = "mm")
     ggsave(paste(file_name, "png", sep = "."), g, dpi = 320, width = 150, height = 150, units = "mm")
 }
